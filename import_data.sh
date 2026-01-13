66906d20d40610d06f74d358557083d7c31dc44c
hwanderer <hwanderer@e.email>
2026-01-13 17:30:19 +0100

    fix downsample

--- import_data.sh
+++ import_data.sh
@@ -82,11 +82,17 @@ import_rasters() {
   # Import with tiling to reduce memory footprint; adjust SRID if your GeoTIFFs are 4326
   docker-compose --env-file .env exec -T postgres bash -c "raster2pgsql -s 3857 -I -C -M -t 200x200 /data/raw/dsm_graz_1m.tif dsm_raster | psql -U postgres -d sonnenbankerl"
   docker-compose --env-file .env exec -T postgres bash -c "raster2pgsql -s 3857 -I -C -M -t 200x200 /data/raw/dem_graz.tif dem_raster | psql -U postgres -d sonnenbankerl"
-  # Downsample DEM to 10m tiles for horizon computation
+  # Downsample DEM to ~10m using native pixel scale, then tile to keep memory low
   docker-compose --env-file .env exec -T postgres psql -U postgres -d sonnenbankerl -c "\
     DROP TABLE IF EXISTS dem_raster_10m CASCADE; \
     CREATE TABLE dem_raster_10m AS \
-      SELECT ST_Tile(ST_Resample(rast, 10, -10), 512, 512) AS rast FROM dem_raster; \
+      SELECT ST_Tile( \
+               ST_Resample( \
+                 rast, \
+                 abs(ST_ScaleX(rast)) * 10, \
+                 abs(ST_ScaleY(rast)) * 10 \
+               ), 512, 512) AS rast \
+      FROM dem_raster; \
     CREATE INDEX IF NOT EXISTS idx_dem10_st_convexhull ON dem_raster_10m USING GIST (ST_ConvexHull(rast)); \
   "
   $PSQL_CMD -f /precomputation/02_import_rasters.sql
